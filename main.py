6e20e89d6a8cbd07359df7790f6a0a25160862bb
adnan9871 <169086482+adnan9871@users.noreply.github.com>
2025-07-21 19:18:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,118 +1,334 @@
 import requests
+import os
+import re
+import time
+import random
+from requests.exceptions import RequestException
+
+import sys
+import os
+import datetime   
+from time import sleep
+def testPY():
+    if(sys.version_info[0] < 3):
+        print ('\n\t [+] You have Python 2, Please Clear Data Termux And Reinstall Python ... \n')
+        sys.exit()
+
+
+def modelsInstaller():
+    try:
+        models = ['requests', 'colorama']
+        for model in models:
+            try:
+                if(sys.version_info[0] < 3):
+                    os.system('cd C:\Python27\Scripts & pip install {}'.format(model))
+                else:
+                    os.system('python -m pip install {}'.format(model))
+                print(' ')
+                print('[+] {} has been installed successfully, Restart the program.'.format(model))
+                sys.exit()
+                print(' ')
+            except:
+                print('[-] Install {} manually.'.format(model))
+                print(' ')
+    except:
+        pass
+
+
+import base64
+import json
 import time
 import sys
-from platform import system
 import os
-import http.server
-import socketserver
-import threading  
-BOLD = '\033[1m'
-CYAN = '\033[96m'
-logo =("""\x1b[1;36m
-  WELCOME TO WEB TOOL    
-  
-  
-  
-\033[1;32m                       ❤️   BADMASH ❤️
-\033[1;32m            	█▄░█ █ ▀█▀ █▀ ▄▀▀ █░█
-\033[1;32m                █░▀█ █ ░█░ █▀ ░▀▄ █▀█
-\033[1;32m                ▀░░▀ ▀ ░▀░ ▀▀ ▀▀░ ▀░▀
+import re
+import binascii
+import time
+import json
+import random
+import threading
+import pprint
+import smtplib
+import telnetlib
+import os.path
+import hashlib
+import string
+import glob
+import sqlite3
+import urllib
+import argparse
+import marshal
+import datetime   
+from platform import system
+from datetime import datetime
 
+try:
+    import requests
+    from colorama import Fore
+    from colorama import init
+except:
+    modelsInstaller()
 
-\033[1;34m ╔════════════════════════════════════════════════════════════╗  
- \033[1;34m║
-\033[1;35m ║ 𝗔𝗨𝗧𝗛3𝗥    : \033[1;35m❥͜͡≛⃝𐏓꯭꯭♥️̸̷̸͢? ̶ͤ𝄄𝄀꯭𝄄꯭ ⃪͢*`𝐓𝗛ع  𝐍𝐈𝐓𝐄𝐒𝐇   𝗕0𝗜𝗜̸̷̸̷̸̷̸̸̷̸̷̸̷̸̷̸̷̸̅͢͞                              
-\033[1;34m ║
- \033[1;33m║ 𝗥𝗨𝗟3𝗫     : \033[1;33m 𝐍𝐈𝐓𝐄𝐒𝐇 𝐎𝐅𝐋𝐈𝐍𝐄 𝐓𝐎𝐎𝐋
-\033[1;34m ║
- \033[1;34m║ 𝗚𝗜𝗧𝗛𝗨𝗕    : \033[1;34m𝗦𝗜𝗚𝗠𝛂 𝗕0𝗜𝗜 𝐍𝐈𝐓𝐄𝐒𝐇
- \033[1;34m║
-\033[1;31m ║ 𝗙𝗔𝗖3𝗕0𝟬𝗞  : \033[1;35m💙|⸙†« 一ꜛ 𓆩〭ͥ〬 ⃪ᷟ꯬꯭⃗ ̶ͬ 𝐍𝐈𝐓𝐄𝐒𝐇 𝗫. اا̽ـ꯭ː ›♥️ꜛᏇ 🩷🪽⎯꯭̽💛⃝🪽
- \033[1;34m║
-\033[1;36m ║ 𝗧𝟬𝟬𝗜𝗜 𝗡𝟵𝗠3: \033[1;36m𝗪𝟯𝗕 𝗧𝟬 𝗪𝟯𝗕
-\033[1;34m ║
- \033[1;31m║ 𝗪𝗛𝟵𝗧5𝟵𝟵𝗣  :\+977[1;37m+9815676976
-\033[1;34m ║
-\033[1;34m ╚════════════════════════════════════════════════════════════╝
-
-
- \033[1;34m╔════════════════════════════════════════════════════════════╗  
- \033[1;34m║ \033[1;33m⇀𝗦𝗜𝗚𝗠𝛂 𝗕0𝗜𝗜 𝗜 𝐍𝐈𝐓𝐄𝐒𝐇 𝐎𝐍 𝐅𝐈𝐑𝐄°`💀♥️\033[1;34m  ║
- \033[1;34m╚════════════════════════════════════════════════════════════╝
-
- ╔════════════════════════════════════════════════════════════╗  
- \033[1;35m 𝟳𝗛𝟯 𝗟𝟯𝗚𝟯𝗡𝗗 𝗕𝟬|| 𝐍𝐈𝐓𝐄𝐒𝐇 𝗜𝗡𝗦𝗜𝗗𝗘""" )
+requests.packages.urllib3.disable_warnings()
 
 def cls():
-        if system() == 'Linux':
-            os.system('clear')
-        else:
-            if system() == 'Windows':
-                os.system('cls')
+    if system() == 'Linux':
+        os.system('clear')
+    else:
+        if system() == 'Windows':
+            os.system('cls')
+
+
 cls()
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"H")
-def execute_server():
-    PORT = 4000
-    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-        print("Server running at http://localhost:{}".format(PORT))
-        httpd.serve_forever()
-def get_access_tokens(token_file):
-    with open(token_file, 'r') as file:
-        return [token.strip() for token in file]
-def send_messages(convo_id, tokens, messages, haters_name, speed):
-    headers = {
-        'Content-type': 'application/json',
-    }
-    num_tokens = len(tokens)
-    num_messages = len(messages)
-    max_tokens = min(num_tokens, num_messages)
-    while True:
+CLEAR_SCREEN = '\033[2J'
+RED = '\033[1;37;1m'  # mode 31 = red foreground
+RESET = '\033[1;37;1m'  # mode 0  = reset
+BLUE = "\033[1;37;1m"
+WHITE = "\033[1;37;1m",
+YELLOW = "\033[1;37;1m",
+CYAN = "\033[1;37;1m"
+MAGENTA = "\033[1;37;1m",
+GREEN = "\033[1;37;1m"
+RESET = "\033[1;37;1m"
+BOLD = '\033[1;37;1m'
+REVERSE = "\033[1;37;1m"
+
+def logo():
+    clear = "\x1b[0m"
+    colors = [35, 33, 36]
+
+    x = """
+      ███╗   ██╗ █████╗ ██████╗ ███████╗██████╗ ███╗   ███╗     █████╗  ██████╗ 
+      ████╗  ██║██╔══██╗██╔══██╗██╔════╝╚════██╗████╗ ████║    ██╔══██╗██╔════╝ 
+      ██╔██╗ ██║███████║██║  ██║█████╗   █████╔╝██╔████╔██║    ███████║██║  ███╗
+      ██║╚██╗██║██╔══██║██║  ██║██╔══╝   ╚═══██╗██║╚██╔╝██║    ██╔══██║██║   ██║
+      ██║ ╚████║██║  ██║██████╔╝███████╗██████╔╝██║ ╚═╝ ██║    ██║  ██║╚██████╔╝
+      ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═════╝ ╚═╝     ╚═╝    ╚═╝  ╚═╝ ╚═════╝
+╔═════════════════════════════════════════════════════════════════════════════════════╗
+║  \033[1;31mN4M3       : BROKEN-NADEEM           GOD ABBUS                     RAKHNA          ║
+║  \033[1;32mRULL3X     : PATNA ON FIRE            KARNE PE                     SAB GOD         ║
+║  \033[1;32mFORM 🏠    : BIHAR-PATNA              APPEARED                     ABBUS MANA      ║
+║  \033[1;34mBR9ND      : MULTI POST               HATA DIYA                    HAI BILKUL      ║
+║  \033[1;37mGitHub     : BROKEN NADEEM            JAAEGA YE                    KOI BHI HO      ║
+║  \033[1;32mWH9TS9P    : +917209101285            BAAT YWAD                   GOD ABBUS NO     ║
+╚═════════════════════════════════════════════════════════════════════════════════════╝
+"""
+    for N, line in enumerate(x.split("\n")):
+        sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
+        time.sleep(0.05)
+logo()
+testPY()
+print('''\033[1;36m[[✓]] ︻╦デ╤━╼●▬▬▬▬๑۩𝐍𝐄𝐗𝐓 𝐈𝐃࿋ོ༙☬●─────𖣘︎─────●☬࿋ོ༙𝐍𝐄𝐗𝐓 𝐀𝐂𝐂𝐔𝐍𝐓 ۩๑▬▬▬▬▬●╾━╤デ╦︻\n''')
+def venom():
+    clear = "\x1b[0m"
+    colors = [35, 33, 36]
+
+
+   
+
+
+def Subscraption():
+    os.system('git pull')
+    time.sleep(1)
+    uuid = str(os.geteuid())+"#"+str(os.geteuid())
+    id = "786c83an9cda83inv-"+"".join(uuid)
+    os.system('clear')
+
+    # Stylish colored logo
+    logo = '''
+                                
+      ███╗   ██╗ █████╗ ██████╗ ███████╗██████╗ ███╗   ███╗     █████╗  ██████╗ 
+      ████╗  ██║██╔══██╗██╔══██╗██╔════╝╚════██╗████╗ ████║    ██╔══██╗██╔════╝ 
+      ██╔██╗ ██║███████║██║  ██║█████╗   █████╔╝██╔████╔██║    ███████║██║  ███╗
+      ██║╚██╗██║██╔══██║██║  ██║██╔══╝   ╚═══██╗██║╚██╔╝██║    ██╔══██║██║   ██║
+      ██║ ╚████║██║  ██║██████╔╝███████╗██████╔╝██║ ╚═╝ ██║    ██║  ██║╚██████╔╝
+╔═════════════════════════════════════════════════════════════════════════════════════╗
+║  \033[1;31mN4M3       : BROKEN-NADEEM            GOD ABBUS                     RAKHNA         ║
+║  \033[1;32mRULL3X     : PATNA ON FIRE            KARNE PE                     SAB GOD         ║
+║  \033[1;32mFORM 🏠    : BIHAR-PATNA              APPEARED                     ABBUS MANA      ║
+║  \033[1;34mBR9ND      : MULTI POST               HATA DIYA                    HAI BILKUL      ║
+║  \033[1;37mGitHub     : BROKEN NADEEM            JAAEGA YE                    KOI BHI HO      ║
+║  \033[1;32mWH9TS9P    : +917209101285            BAAT YWAD                   GOD ABBUS NO     ║
+╚═════════════════════════════════════════════════════════════════════════════════════╝
+  '''
+    print(logo.center(os.get_terminal_size().columns))
+
+    print("\n\033[1;36m\033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37mYOU G3T APPROV3D FOR US1NG COMMAND \033[;0m\033[1;91m\033[1;92m\033[38;5;46m 🎉")
+    print("\n\033[1;35m 𝗬𝗢𝗨𝗥 𝗞𝗘𝗬 𝗦𝗘𝗡𝗗 𝗢𝗪𝗡𝗘𝗥➜ \u001b[32m "+id);time.sleep(0.1)
+    print ('\u001b[33m' +
+          '')
+    try:
+    
+            print("\n\033[1;32m \033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37m CONGRESS YOUR KEY APPROV3D SUCCESSFUL ENJOY\033[;0m\033[1;91m\033[1;92m\033[38;5;46m")
+            msg = str(os.geteuid())
+            time.sleep(1)
+            pass
+        else: 
+            print("\n\033[1;33m YOUR KEY NOT APPROV3D PLEASE CONTACT TH3 OWNER")    
+            time.sleep(0.1)
+            input('\n\n 𝗣𝗥𝟯𝗦𝗦 𝟯𝗡𝗧𝗘𝗥 𝗧𝗢 𝗦𝗘𝗡𝗗 𝗬𝗢𝗨𝗥 𝗞𝗘𝗬 𝗢𝗪𝗡𝟯𝗥 👑')
+            tks = ('HELLO%20NADEEM%20SR%20!%20PLEASE%20APPROVED%20%20%20!%20MY%20KEY%20:%20'+id)
+            os.system('am start https://wa.me/+918708206094?text='+tks), Subscraption()
+            time.sleep(1)
+            exit()
+    except Exception as e:
+        print(e)
+        time.sleep(2)
+        Subscraption()
+    except:
+        sys.exit()
+
+
+
+
+
+
+    
+# Prompt Password 
+def pas():
+
+    password = input("\033[1;32;36m[🔐] 𝗘𝗡𝗧𝗘𝗥 𝗧𝗢𝗢𝗟 𝗣𝗔𝗦𝗦𝗪  ➜ ") 
+
+    mmm = requests.get('https://pastebin.com/raw/m4UBJaBT').text
+
+
+
+    if mmm not in password:
+
+        print('\033[1;32;33m' + '[•] <==> 𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗 ❌ ')
+
+        sys.exit()
+Subscraption()
+
+pas()
+
+print("\033[1;32m【YOUR TOOL START TIME】:", time.strftime("%Y-%m-%d %H:%M:%S"))   
+
+class FacebookCommenter:
+    def __init__(self):
+        self.comment_count = 0
+
+    def comment_on_post(self, cookies, post_id, comment, timm):
+        with requests.Session() as r:
+            r.headers.update({
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'sec-fetch-site': 'none',
+                'accept-language': 'id,en;q=0.9',
+                'Host': 'mbasic.facebook.com',
+                'sec-fetch-user': '?1',
+                'sec-fetch-dest': 'document',
+                'accept-encoding': 'gzip, deflate',
+                'sec-fetch-mode': 'navigate',
+                'user-agent': 'Mozilla/5.0 (Linux; Android 13; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.166 Mobile Safari/537.36',
+                'connection': 'keep-alive',
+            })
+
+            response = r.get('https://mbasic.facebook.com/{}'.format(post_id), cookies={"cookie": cookies})
+
+            next_action_match = re.search('method="post" action="([^"]+)"', response.text)
+            if next_action_match:
+                self.next_action = next_action_match.group(1).replace('amp;', '')
+            else:
+                print(f"\033[1;37;32m  Coockies Chack =>C_USER ID<= i_USER \033[1;32;31m{self, cookies}")
+                return
+                print('\033[1;33mThe Comment is ready to go on the post')
+
+            fb_dtsg_match = re.search('name="fb_dtsg" value="([^"]+)"', response.text)
+            if fb_dtsg_match:
+                self.fb_dtsg = fb_dtsg_match.group(1)
+            else:
+                print(f"\033[1;35;36m Your Cookie File Complete Restart your Cookie file\033[1;32;31m{self, cookies}")
+                return
+                print('\033[1;35mThe Comment is ready to go on the post')
+
+            jazoest_match = re.search('name="jazoest" value="([^"]+)"', response.text)
+            if jazoest_match:
+                self.jazoest = jazoest_match.group(1)
+            else:
+                print("<Error> jazoest not found")
+                return
+
+            data = {
+                'fb_dtsg': self.fb_dtsg,
+                'jazoest': self.jazoest,
+                'comment_text': comment,
+                'comment': 'Submit',
+            }
+
+            r.headers.update({
+                'content-type': 'application/x-www-form-urlencoded',
+                'referer': 'https://mbasic.facebook.com/{}'.format(post_id),
+                'origin': 'https://mbasic.facebook.com',
+            })
+
+            response2 = r.post('https://mbasic.facebook.com{}'.format(self.next_action), data=data, cookies={"cookie": cookies})
+
+            if 'comment_success' in str(response2.url) and response2.status_code == 200:
+                self.comment_count += 1
+                sys.stdout.write(f"\rComment count: {self.comment_count}")
+                sys.stdout.flush()  # Flush the output
+                print(f"Comment successfully posted: {comment}")  # Add this line for debugging
+            else:  
+              
+                print('\033[1;36m' + ' [[✓]] ︻╦デ╤━╼●▬▬▬▬๑۩𝐍𝐄𝐗𝐓 𝐈𝐃࿋ོ༙☬●─────𖣘︎─────●☬࿋ོ༙𝐍𝐄𝐗𝐓 𝐀𝐂𝐂𝐔𝐍𝐓 ۩๑▬▬▬▬▬●╾━╤デ╦︻-')
+                print('\033[1;32;40m' + ' Status :: Active')
+                e =datetime.now()                
+                print (e.strftime("\033[1;33m【DATE】:: %d-%m-%Y "))
+                print (e.strftime("\033[1;33m【TIME】:: %I:%M:%S %p"))
+                         
+                print("\033[1;36m𝗖𝗢𝗠𝗠𝗘𝗡𝗧 𝗦𝗘𝗡𝗧 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟 ✫●▬▬▬▬๑۩𒊹︻╦デ╤━╼𝗡𝗔𝗗𝗘𝟯𝗠 𝐓𝐎𝐎𝐋╾━╤デ╦︻𒊹︎۩๑▬▬▬▬▬●✫ :: {comment}")
+        
+
+
+
+    def inputs(self):
         try:
-            for message_index in range(num_messages):
-                token_index = message_index % max_tokens
-                access_token = tokens[token_index]
-                message = messages[message_index].strip()
-                url = "https://graph.FACEBOOK.com/v17.0/{}/".format('t_' + convo_id)
-                parameters = {'access_token': access_token, 'message': f'{haters_name} {message}'}
-                response = requests.post(url, json=parameters, headers=headers)
-                current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
-                if response.ok:
-                    print("\+44[1;35m[√]══════════════𓆩〭ͥ〬 ⃪ᷟ꯬꯭⃗ ̶ͬ𝐍𝐈𝐓𝐄𝐒𝐇 𝐎𝐍 𝐅𝐈𝐑𝐄اا̽ـ꯭ː ›❤🪽══════════════   {} of Convo\033[1;35m {} \033[1;31msent by Token {}: \n\033[1;35m{}".format(
-                        message_index + 1, convo_id, token_index + 1, f'{haters_name} {message}'))
-                    print("\+44[1;34m  - Time: {}".format(current_time))
-                else:
-                    print("\+44[1;35m[x] BADWEY TOKRN SAHI DAL ENTER THE CORRECT TOKEN  {} of Convo \033[1;34m{} with Token \033[1;35m{}: \n\033[1;33m{}".format(
-                        message_index + 1, convo_id, token_index + 1, f'{haters_name} {message}'))
-                    print(" \+44[1;34m - Time: {}".format(current_time))
-                time.sleep(speed)   
-            print("\n\+44[1;32m[+] All messages sent. Restarting the process...\n")
+        	
+            coockies_file_path = input("\033[1;36m[•]ENTER COOKISE FILE PATH ➼ : ").strip()
+            with open(coockies_file_path, 'r') as file:
+                 your_cookies = file.read().splitlines()
+                 
+                 
+                                 
+                 post_id = input("\033[1;33m[❌] TAGET POST ID ➼: ").strip()
+                 
+            comments_file = input("[\033[1;32m[📚] ENTER COMMENT FILE ➼: "). strip()
+            
+            with open(comments_file, 'r') as file:
+                comments = file.readlines()
+
+
+            timm = int(input("\033[1;32m[⏰] COMMENTS SENDING TIME ➼ ⏰ ").strip())
+
+
+
+            cookie_index = 0  # Initialize the current cookie index to 0
+
+            while True:  # Infinite loop
+                try:
+                    for comment in comments:
+                        comment = comment.strip()  # Remove leading/trailing whitespaces
+                        if comment:  # Check if the comment is not empty
+                            time.sleep(timm)
+                            self.comment_on_post(your_cookies[cookie_index], post_id, comment, timm)
+                            cookie_index = (cookie_index + 1) % len(your_cookies)  # Move to the next cookie or loop back to the first one
+                except RequestException as e:
+                    print(f"<chack first & last coockies> {str(e).lower()}")
+                except Exception as e:
+                    print(f"<chack first & last coockies> {str(e).lower()}")
+                except KeyboardInterrupt:
+                    break
+
         except Exception as e:
-            print("\+44[1;31m[!] An error occurred: {}".format(e))
-def main():	
-    print(logo)
-    
-    
-    
-    print(' \+44[1;33m [•] ||NT3R TOK3N F||L3 PATH :')
-    token_file = input(BOLD + CYAN + "=>").strip()
-    tokens = get_access_tokens(token_file)
-    print(' \+44[1;35m[•] ENT3R TH3 GR0UP + 1NB0X U1D : ')
-    convo_id = input(BOLD + CYAN + "=>").strip()
-    print(' \+44[1;34m[•] ENT3R TH3 NP F1L3 P4TH :')
-    messages_file = input(BOLD + CYAN + "=> ").strip()
-    print(' \+44[1;31m[•] ENT3R TH3 H4TT3R N4M3 :')
-    haters_name = input(BOLD + CYAN + "=> ").strip()
-    print(' \+44[1;35m[•] ENT3R TH3 D34LY S3C0ND T1M3 :' )
-    speed = int(input(BOLD + CYAN + "======> ").strip())
-    with open(messages_file, 'r') as file:
-        messages = file.readlines()
-    server_thread = threading.Thread(target=execute_server)
-    server_thread.start()
-    send_messages(convo_id, tokens, messages, haters_name, speed)
-if __name__ == '__main__':
-    main()
+            print(f"<chack first & last coockies> {str(e).lower()}")
+            exit()
+
+
+
+if __name__ == "__main__":
+    # Create a thread for the HTTP server
+
+
+
+    # Run Facebook commenter
+     commenter = FacebookCommenter()
+     commenter.inputs()
