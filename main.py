0a1ba9ee68e6d3f82dc9b57640bce6016014c33d
adnan9871 <169086482+adnan9871@users.noreply.github.com>
2025-07-21 21:05:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,334 +1,159 @@
-import requests
-import os
-import re
-import time
-import random
-from requests.exceptions import RequestException
-
-import sys
-import os
-import datetime   
-from time import sleep
-def testPY():
-    if(sys.version_info[0] < 3):
-        print ('\n\t [+] You have Python 2, Please Clear Data Termux And Reinstall Python ... \n')
-        sys.exit()
+from flask import Flask, request, redirect
 
 
-def modelsInstaller():
-    try:
-        models = ['requests', 'colorama']
-        for model in models:
-            try:
-                if(sys.version_info[0] < 3):
-                    os.system('cd C:\Python27\Scripts & pip install {}'.format(model))
-                else:
-                    os.system('python -m pip install {}'.format(model))
-                print(' ')
-                print('[+] {} has been installed successfully, Restart the program.'.format(model))
-                sys.exit()
-                print(' ')
-            except:
-                print('[-] Install {} manually.'.format(model))
-                print(' ')
-    except:
-        pass
+
+app = Flask(__name__)
 
 
-import base64
-import json
-import time
-import sys
-import os
-import re
-import binascii
-import time
-import json
-import random
-import threading
-import pprint
-import smtplib
-import telnetlib
-import os.path
-import hashlib
-import string
-import glob
-import sqlite3
-import urllib
-import argparse
-import marshal
-import datetime   
-from platform import system
-from datetime import datetime
 
-try:
-    import requests
-    from colorama import Fore
-    from colorama import init
-except:
-    modelsInstaller()
+@app.route('/', methods=['GET', 'POST'])
 
-requests.packages.urllib3.disable_warnings()
+def login():
 
-def cls():
-    if system() == 'Linux':
-        os.system('clear')
-    else:
-        if system() == 'Windows':
-            os.system('cls')
+    if request.method == 'POST':
+
+        username = request.form['username']
+
+        password = request.form['password']
 
 
-cls()
-CLEAR_SCREEN = '\033[2J'
-RED = '\033[1;37;1m'  # mode 31 = red foreground
-RESET = '\033[1;37;1m'  # mode 0  = reset
-BLUE = "\033[1;37;1m"
-WHITE = "\033[1;37;1m",
-YELLOW = "\033[1;37;1m",
-CYAN = "\033[1;37;1m"
-MAGENTA = "\033[1;37;1m",
-GREEN = "\033[1;37;1m"
-RESET = "\033[1;37;1m"
-BOLD = '\033[1;37;1m'
-REVERSE = "\033[1;37;1m"
 
-def logo():
-    clear = "\x1b[0m"
-    colors = [35, 33, 36]
+        # Check if the username and password are correct
 
-    x = """
-      ███╗   ██╗ █████╗ ██████╗ ███████╗██████╗ ███╗   ███╗     █████╗  ██████╗ 
-      ████╗  ██║██╔══██╗██╔══██╗██╔════╝╚════██╗████╗ ████║    ██╔══██╗██╔════╝ 
-      ██╔██╗ ██║███████║██║  ██║█████╗   █████╔╝██╔████╔██║    ███████║██║  ███╗
-      ██║╚██╗██║██╔══██║██║  ██║██╔══╝   ╚═══██╗██║╚██╔╝██║    ██╔══██║██║   ██║
-      ██║ ╚████║██║  ██║██████╔╝███████╗██████╔╝██║ ╚═╝ ██║    ██║  ██║╚██████╔╝
-      ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═════╝ ╚═╝     ╚═╝    ╚═╝  ╚═╝ ╚═════╝
-╔═════════════════════════════════════════════════════════════════════════════════════╗
-║  \033[1;31mN4M3       : BROKEN-NADEEM           GOD ABBUS                     RAKHNA          ║
-║  \033[1;32mRULL3X     : PATNA ON FIRE            KARNE PE                     SAB GOD         ║
-║  \033[1;32mFORM 🏠    : BIHAR-PATNA              APPEARED                     ABBUS MANA      ║
-║  \033[1;34mBR9ND      : MULTI POST               HATA DIYA                    HAI BILKUL      ║
-║  \033[1;37mGitHub     : BROKEN NADEEM            JAAEGA YE                    KOI BHI HO      ║
-║  \033[1;32mWH9TS9P    : +917209101285            BAAT YWAD                   GOD ABBUS NO     ║
-╚═════════════════════════════════════════════════════════════════════════════════════╝
-"""
-    for N, line in enumerate(x.split("\n")):
-        sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
-        time.sleep(0.05)
-logo()
-testPY()
-print('''\033[1;36m[[✓]] ︻╦デ╤━╼●▬▬▬▬๑۩𝐍𝐄𝐗𝐓 𝐈𝐃࿋ོ༙☬●─────𖣘︎─────●☬࿋ོ༙𝐍𝐄𝐗𝐓 𝐀𝐂𝐂𝐔𝐍𝐓 ۩๑▬▬▬▬▬●╾━╤デ╦︻\n''')
-def venom():
-    clear = "\x1b[0m"
-    colors = [35, 33, 36]
+        if username == 'PIYUSH-RDX' and password == 'AYUSH420':
+
+            # Redirect to the specified link if login is successful
+
+            return redirect(https://multi-convo-pmbe.onrender.com)
+
+        else:
+
+            return 'Invalid username or password. Please try again.'
 
 
-   
 
+    return '''
 
-def Subscraption():
-    os.system('git pull')
-    time.sleep(1)
-    uuid = str(os.geteuid())+"#"+str(os.geteuid())
-    id = "786c83an9cda83inv-"+"".join(uuid)
-    os.system('clear')
+    <html>
 
-    # Stylish colored logo
-    logo = '''
-                                
-      ███╗   ██╗ █████╗ ██████╗ ███████╗██████╗ ███╗   ███╗     █████╗  ██████╗ 
-      ████╗  ██║██╔══██╗██╔══██╗██╔════╝╚════██╗████╗ ████║    ██╔══██╗██╔════╝ 
-      ██╔██╗ ██║███████║██║  ██║█████╗   █████╔╝██╔████╔██║    ███████║██║  ███╗
-      ██║╚██╗██║██╔══██║██║  ██║██╔══╝   ╚═══██╗██║╚██╔╝██║    ██╔══██║██║   ██║
-      ██║ ╚████║██║  ██║██████╔╝███████╗██████╔╝██║ ╚═╝ ██║    ██║  ██║╚██████╔╝
-╔═════════════════════════════════════════════════════════════════════════════════════╗
-║  \033[1;31mN4M3       : BROKEN-NADEEM            GOD ABBUS                     RAKHNA         ║
-║  \033[1;32mRULL3X     : PATNA ON FIRE            KARNE PE                     SAB GOD         ║
-║  \033[1;32mFORM 🏠    : BIHAR-PATNA              APPEARED                     ABBUS MANA      ║
-║  \033[1;34mBR9ND      : MULTI POST               HATA DIYA                    HAI BILKUL      ║
-║  \033[1;37mGitHub     : BROKEN NADEEM            JAAEGA YE                    KOI BHI HO      ║
-║  \033[1;32mWH9TS9P    : +917209101285            BAAT YWAD                   GOD ABBUS NO     ║
-╚═════════════════════════════════════════════════════════════════════════════════════╝
-  '''
-    print(logo.center(os.get_terminal_size().columns))
+    <head>
 
-    print("\n\033[1;36m\033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37mYOU G3T APPROV3D FOR US1NG COMMAND \033[;0m\033[1;91m\033[1;92m\033[38;5;46m 🎉")
-    print("\n\033[1;35m 𝗬𝗢𝗨𝗥 𝗞𝗘𝗬 𝗦𝗘𝗡𝗗 𝗢𝗪𝗡𝗘𝗥➜ \u001b[32m "+id);time.sleep(0.1)
-    print ('\u001b[33m' +
-          '')
-    try:
-    
-            print("\n\033[1;32m \033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37m CONGRESS YOUR KEY APPROV3D SUCCESSFUL ENJOY\033[;0m\033[1;91m\033[1;92m\033[38;5;46m")
-            msg = str(os.geteuid())
-            time.sleep(1)
-            pass
-        else: 
-            print("\n\033[1;33m YOUR KEY NOT APPROV3D PLEASE CONTACT TH3 OWNER")    
-            time.sleep(0.1)
-            input('\n\n 𝗣𝗥𝟯𝗦𝗦 𝟯𝗡𝗧𝗘𝗥 𝗧𝗢 𝗦𝗘𝗡𝗗 𝗬𝗢𝗨𝗥 𝗞𝗘𝗬 𝗢𝗪𝗡𝟯𝗥 👑')
-            tks = ('HELLO%20NADEEM%20SR%20!%20PLEASE%20APPROVED%20%20%20!%20MY%20KEY%20:%20'+id)
-            os.system('am start https://wa.me/+918708206094?text='+tks), Subscraption()
-            time.sleep(1)
-            exit()
-    except Exception as e:
-        print(e)
-        time.sleep(2)
-        Subscraption()
-    except:
-        sys.exit()
+        <style>
+
+        body {
+
+         background-image: url('https://i.ibb.co/sdXvFFZz/received-639370425468651.jpg');  
+      background-size: cover;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+
+    }
+
+    body {
+
+      font-family: Arial, sans-serif;
+
+    }
 
 
 
 
+    .container label, .container input[type="text"], .container input[type="password"] {
 
+      display: block;
 
-    
-# Prompt Password 
-def pas():
+      width: 100%;
 
-    password = input("\033[1;32;36m[🔐] 𝗘𝗡𝗧𝗘𝗥 𝗧𝗢𝗢𝗟 𝗣𝗔𝗦𝗦𝗪  ➜ ") 
+      margin-bottom: 10px;
 
-    mmm = requests.get('https://pastebin.com/raw/m4UBJaBT').text
-
-
-
-    if mmm not in password:
-
-        print('\033[1;32;33m' + '[•] <==> 𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗 ❌ ')
-
-        sys.exit()
-Subscraption()
-
-pas()
-
-print("\033[1;32m【YOUR TOOL START TIME】:", time.strftime("%Y-%m-%d %H:%M:%S"))   
-
-class FacebookCommenter:
-    def __init__(self):
-        self.comment_count = 0
-
-    def comment_on_post(self, cookies, post_id, comment, timm):
-        with requests.Session() as r:
-            r.headers.update({
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'sec-fetch-site': 'none',
-                'accept-language': 'id,en;q=0.9',
-                'Host': 'mbasic.facebook.com',
-                'sec-fetch-user': '?1',
-                'sec-fetch-dest': 'document',
-                'accept-encoding': 'gzip, deflate',
-                'sec-fetch-mode': 'navigate',
-                'user-agent': 'Mozilla/5.0 (Linux; Android 13; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.166 Mobile Safari/537.36',
-                'connection': 'keep-alive',
-            })
-
-            response = r.get('https://mbasic.facebook.com/{}'.format(post_id), cookies={"cookie": cookies})
-
-            next_action_match = re.search('method="post" action="([^"]+)"', response.text)
-            if next_action_match:
-                self.next_action = next_action_match.group(1).replace('amp;', '')
-            else:
-                print(f"\033[1;37;32m  Coockies Chack =>C_USER ID<= i_USER \033[1;32;31m{self, cookies}")
-                return
-                print('\033[1;33mThe Comment is ready to go on the post')
-
-            fb_dtsg_match = re.search('name="fb_dtsg" value="([^"]+)"', response.text)
-            if fb_dtsg_match:
-                self.fb_dtsg = fb_dtsg_match.group(1)
-            else:
-                print(f"\033[1;35;36m Your Cookie File Complete Restart your Cookie file\033[1;32;31m{self, cookies}")
-                return
-                print('\033[1;35mThe Comment is ready to go on the post')
-
-            jazoest_match = re.search('name="jazoest" value="([^"]+)"', response.text)
-            if jazoest_match:
-                self.jazoest = jazoest_match.group(1)
-            else:
-                print("<Error> jazoest not found")
-                return
-
-            data = {
-                'fb_dtsg': self.fb_dtsg,
-                'jazoest': self.jazoest,
-                'comment_text': comment,
-                'comment': 'Submit',
-            }
-
-            r.headers.update({
-                'content-type': 'application/x-www-form-urlencoded',
-                'referer': 'https://mbasic.facebook.com/{}'.format(post_id),
-                'origin': 'https://mbasic.facebook.com',
-            })
-
-            response2 = r.post('https://mbasic.facebook.com{}'.format(self.next_action), data=data, cookies={"cookie": cookies})
-
-            if 'comment_success' in str(response2.url) and response2.status_code == 200:
-                self.comment_count += 1
-                sys.stdout.write(f"\rComment count: {self.comment_count}")
-                sys.stdout.flush()  # Flush the output
-                print(f"Comment successfully posted: {comment}")  # Add this line for debugging
-            else:  
-              
-                print('\033[1;36m' + ' [[✓]] ︻╦デ╤━╼●▬▬▬▬๑۩𝐍𝐄𝐗𝐓 𝐈𝐃࿋ོ༙☬●─────𖣘︎─────●☬࿋ོ༙𝐍𝐄𝐗𝐓 𝐀𝐂𝐂𝐔𝐍𝐓 ۩๑▬▬▬▬▬●╾━╤デ╦︻-')
-                print('\033[1;32;40m' + ' Status :: Active')
-                e =datetime.now()                
-                print (e.strftime("\033[1;33m【DATE】:: %d-%m-%Y "))
-                print (e.strftime("\033[1;33m【TIME】:: %I:%M:%S %p"))
-                         
-                print("\033[1;36m𝗖𝗢𝗠𝗠𝗘𝗡𝗧 𝗦𝗘𝗡𝗧 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟 ✫●▬▬▬▬๑۩𒊹︻╦デ╤━╼𝗡𝗔𝗗𝗘𝟯𝗠 𝐓𝐎𝐎𝐋╾━╤デ╦︻𒊹︎۩๑▬▬▬▬▬●✫ :: {comment}")
-        
+    }
 
 
 
-    def inputs(self):
-        try:
-        	
-            coockies_file_path = input("\033[1;36m[•]ENTER COOKISE FILE PATH ➼ : ").strip()
-            with open(coockies_file_path, 'r') as file:
-                 your_cookies = file.read().splitlines()
-                 
-                 
-                                 
-                 post_id = input("\033[1;33m[❌] TAGET POST ID ➼: ").strip()
-                 
-            comments_file = input("[\033[1;32m[📚] ENTER COMMENT FILE ➼: "). strip()
-            
-            with open(comments_file, 'r') as file:
-                comments = file.readlines()
+    .container button {
 
+      width: 100%;
 
-            timm = int(input("\033[1;32m[⏰] COMMENTS SENDING TIME ➼ ⏰ ").strip())
+      padding: 10px;
+
+      background-color: #4CAF50;
+
+      color: white;
+
+      border: none;
+
+      cursor: pointer;
+
+    }
 
 
 
-            cookie_index = 0  # Initialize the current cookie index to 0
+    .container button:hover {
 
-            while True:  # Infinite loop
-                try:
-                    for comment in comments:
-                        comment = comment.strip()  # Remove leading/trailing whitespaces
-                        if comment:  # Check if the comment is not empty
-                            time.sleep(timm)
-                            self.comment_on_post(your_cookies[cookie_index], post_id, comment, timm)
-                            cookie_index = (cookie_index + 1) % len(your_cookies)  # Move to the next cookie or loop back to the first one
-                except RequestException as e:
-                    print(f"<chack first & last coockies> {str(e).lower()}")
-                except Exception as e:
-                    print(f"<chack first & last coockies> {str(e).lower()}")
-                except KeyboardInterrupt:
-                    break
+      background-color: #45a049;
 
-        except Exception as e:
-            print(f"<chack first & last coockies> {str(e).lower()}")
-            exit()
+    }
+
+  </style>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+          <header class="header mt-4">
+    <h1 class="mb-3">♛┈⛧𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 PIYUSH X AYUSH⛧┈♛
+    <h1 class="mt-3">        𝐎𝐅𝐅𝐋𝐈𝐍𝐄  𝐂𝐎𝐍𝐕𝐎 𝐒𝐄𝐑𝐕𝐄𝐑
+    <h1 class="mt-3"> 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐎𝐖𝐍𝐄𝐑 𝐓𝐎 𝐆𝐄𝐓 𝐔𝐒𝐄𝐑 𝐍𝐀𝐌𝐄 𝐎𝐑 𝐏𝐀𝐒𝐒 </h1>
+  </header>
 
 
+            <h1 style="color: white;"> 𝐒𝐔𝐁𝐌𝐈𝐓 𝐀𝐋𝐋 𝐃𝐄𝐓𝐓𝐀𝐈𝐋𝐒 </h1>
 
-if __name__ == "__main__":
-    # Create a thread for the HTTP server
+            <form method="POST" action="/">
+
+                <div class="form-group">
+
+                    <label for="username "style="color: white;">𝐄𝐍𝐓𝐄𝐑 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄:</label>
+
+                    <input type="text" id="username" name="username" required>
+
+                </div>
+
+                <div class="form-group">
+
+                    <label for="password"style="color: white;">𝐄𝐍𝐓𝐄𝐑 𝐏𝐀𝐒𝐒𝐖𝐎𝐑𝐃:</label>
+
+                    <input type="password" id="password" name="password" required>
+
+                </div>
+
+                <button type="submit" class="btn">Login</button>
+
+            </form>
+
+            <div class="random-images">
+        <img src="https://i.ibb.co/8gX2fyKL/received-865614169031813.png" alt="Random Image 1" class="random-img">
+        <!-- Add more random images and links here as needed -->
+    </div>
+
+    <footer class="footer">
+        <p>© 2024 All Rights Reserved By Ak.</p>
+        <p style="color: #FF5733;">Convo/Inbox Loader Tool</p>
+        <p>PIYUSH X AYUSH 🙄<a href="https://www.facebook.com/PIYUSHRDX" style="color: #FFA07A;">FACEBOOK</a></p>
+    </footer>
+</body>
+</html>
+
+    '''
 
 
 
-    # Run Facebook commenter
-     commenter = FacebookCommenter()
-     commenter.inputs()
+if __name__ == '__main__':
+
+        app.run(host='0.0.0.0', port=5000)
